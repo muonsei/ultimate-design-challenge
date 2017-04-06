2e6306717ca6a4cc5b20e22db18e5dca6e3d62f9
Jolene Valenzuela <atashinosoundlessvoice@gmail.com>
2017-04-06 11:54:53 +0800

    Coded by Pons

---
+++ src/model/AppointmentFetcher.java
@@ -0,0 +1,89 @@
+package Model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class AppointmentFetcher implements I_Fetcher {
+	private DBConnection connection;
+	
+	public AppointmentFetcher() {
+		connection = new DBConnection();
+		connection.getConnection();
+	}
+	
+	@Override
+	public ArrayList<Appointment> getAll() {
+		ArrayList<Appointment> appointmentList = new ArrayList<Appointment>();
+		
+		try {
+			ResultSet rs;
+			String query = "SELECT * FROM appointment";
+			
+			Statement stment = connection.getConnection().createStatement();
+			rs = stment.executeQuery(query);
+			
+			while (rs.next()) {
+				Client client = toObject(rs);
+				clientList.add(client);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return appointmentList;
+	}
+	
+	@Override
+	public ArrayList<Appointment> getBySearch(String keyword) { //Client
+		ArrayList<Appointment> appointmentList = new ArrayList<Appointment>();
+		
+		try {
+			ResultSet rs;
+			String query = "SELECT appointment.time, appointment.date FROM appointment, client WHERE appointment.client_id = client.client_id "
+					+ "AND client.client_name LIKE %" + keyword + "%" +
+					" ORDER BY client_name";
+			
+			Statement stment = connection.getConnection().createStatement();
+			rs = stment.executeQuery(query);
+			
+			while (rs.next()) {
+				Appointment appointment = toObject(rs);
+				appointmentList.add(appointment);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return appointmentList;
+	}
+	
+	public ArrayList<Appointment> getByDoctor(String keyword) {
+		ArrayList<Appointment> appointmentList = new ArrayList<Appointment>();
+		
+		try {
+			ResultSet rs;
+			String query = "SELECT appointment.time, appointment.date FROM appointment, doctor WHERE appointment.doctor_id = doctor.doctor_id "
+					+ "AND doctor.doctor_name LIKE %" + keyword + "%" +
+					" ORDER BY doctor_name";
+			
+			Statement stment = connection.getConnection().createStatement();
+			rs = stment.executeQuery(query);
+			
+			while (rs.next()) {
+				Appointment appointment = toObject(rs);
+				appointmentList.add(appointment);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return appointmentList;
+	}
+	@Override
+	public Appointment toObject(ResultSet rs) throws SQLException {
+		Appointment appointment = new Appointment(rs.getString("appointment_date"),rs.getString("appointment_time"));
+		return appointment;
+	}
+}
---
+++ src/model/ClientFetcher.java
@@ -0,0 +1,66 @@
+package Model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.Statement;
+
+public class ClientFetcher implements I_Fetcher {
+	private DBConnection connection;
+	
+	public ClientFetcher() {
+		connection = new DBConnection();
+		connection.getConnection();
+	}
+	
+	@Override
+	public ArrayList<Client> getAll() {
+		ArrayList<Client> clientList = new ArrayList<Client>();
+		
+		try {
+			ResultSet rs;
+			String query = "SELECT * FROM client ORDER BY client_name";
+			
+			Statement stment = connection.getConnection().createStatement();
+			rs = stment.executeQuery(query);
+			
+			while (rs.next()) {
+				Client client = toObject(rs);
+				clientList.add(client);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return clientList;
+	}
+
+	@Override
+	public ArrayList<Client> getBySearch(String keyword) {
+		ArrayList<Client> clientList = new ArrayList<Client>();
+		
+		try {
+			ResultSet rs;
+			String query = "SELECT * FROM client WHERE client_name LIKE %" + keyword + "%" +
+					" ORDER BY client_name";
+			
+			Statement stment = connection.getConnection().createStatement();
+			rs = stment.executeQuery(query);
+			
+			while (rs.next()) {
+				Client client = toObject(rs);
+				clientList.add(client);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return clientList;
+	}
+
+	@Override
+	public Client toObject(ResultSet rs) throws SQLException {
+		Client client = new Client(rs.getString("client_name"));
+		return client;
+	}
+}
