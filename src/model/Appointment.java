18306f83956ce4cbba116c768b641d2e9b66cf79
Jolene Valenzuela <atashinosoundlessvoice@gmail.com>
2017-04-11 08:51:03 +0800

    Coded by Kate

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -2,12 +2,40 @@ package model;
 
 public class Appointment {
 	private String date;
-	private String startTime;
-	private String endTime;
+	private String from_time;
+	private String to_time;
+	private String clientname;
 	
-	public Appointment(String date, String startTime, String endTime) {
+	public Appointment(String date, String from_time,String to_time,String clientname) {
 		this.date = date;
-		this.startTime = startTime;
-		this.endTime = endTime;
+		this.from_time = from_time;
+		this.to_time = to_time;
+		this.clientname = clientname;
 	}
+	
+	public void setClientName(String n)
+	{
+		clientname = n;
+	}
+	
+	public String getClientName()
+	{
+		return clientname;
+	}
+	
+	public String getFromTime()
+	{
+		return from_time;
+	}
+	
+	public String getToTime()
+	{
+		return to_time;
+	}
+	
+	public String getDate()
+	{
+		return date;
+	}
+	
 }
