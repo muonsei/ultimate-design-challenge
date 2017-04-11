185a7ecf7bd0df1aa77cb5314247b0fed3aa5613
Jolene Valenzuela <atashinosoundlessvoice@gmail.com>
2017-04-11 08:04:21 +0800

    Added endTime as attribute

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -3,9 +3,11 @@ package model;
 public class Appointment {
 	private String date;
 	private String startTime;
+	private String endTime;
 	
-	public Appointment(String date, String startTime) {
+	public Appointment(String date, String startTime, String endTime) {
 		this.date = date;
 		this.startTime = startTime;
+		this.endTime = endTime;
 	}
 }
